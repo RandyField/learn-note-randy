be422a3e71023fb5a3c80110844b83a4f7e71614
RandyField <287572291@qq.com>
2018-01-16 16:35:09 +0800

    add vue note

--- vue-note.vue
+++ vue-note.vue
@@ -1,13 +1,9 @@
-v-bind
-v-if
-v-else
-v-else-if
-v-for
-v-on
-v-model
-v-once
-v-html
 
+<template>
+	
+</template>
+
+<script>
 
 var app=new Vue({
 	el:'#',
@@ -21,3 +17,42 @@ var app=new Vue({
 	}
 	
 });
+
+// v-bind
+// v-if
+// v-else
+// v-else-if
+// v-for
+// v-on
+// v-model
+// v-once
+// v-html
+
+//设置npm国内淘宝镜像
+// npm install -g cnpm --registry= https://registry.npm.taobao.org
+
+//安装webpack
+//npm install webpack -g
+
+//安装vue-cli脚手架构建工具
+//npm install vue-cli -g
+
+//安装vue脚手架
+//vue init webpack project-name
+
+//安装项目依赖
+//npm install
+
+//安装 vue 路由模块 vue-router 和网络请求模块 vue-resource
+//cnpm install vue-router vue-resource --save。
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
